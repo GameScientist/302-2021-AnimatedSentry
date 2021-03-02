d9648986613d9e4aa72c237c5342c725b575a71f
Tyler Kortge <tylerkortge@charter.net>
2021-03-02 11:52:49 -0500

    Programmed turret to follow the player around and point its gun.

--- Assets/Scripts/SentryTurret.cs
+++ Assets/Scripts/SentryTurret.cs
@@ -11,9 +11,7 @@ public class SentryTurret : MonoBehaviour
     public Transform leg3;
     public Transform leg4;
 
-    public Transform target;
-
-    private float timeActive;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -30,21 +28,23 @@ public class SentryTurret : MonoBehaviour
             switch (state)
         {
             case "Idle":
-                rotator.rotation = AnimMath.Slide(rotator.rotation, Quaternion.Euler(0, 0, 0));
+                rotator.rotation = AnimMath.Slide(rotator.rotation, Quaternion.Euler(270, 0, 0));
                 transform.position = (transform.position.x * transform.right) + (transform.up * AnimMath.Slide(transform.position.y, 0.75f, 0.1f)) + (transform.position.z * transform.forward);
-                leg1.localPosition = (transform.localPosition.x * transform.right) + transform.up * AnimMath.Slide(leg1.localPosition.y, 0, 0.1f) + (transform.localPosition.z * transform.forward);
-                leg2.localPosition = (transform.localPosition.x * transform.right) + transform.up * AnimMath.Slide(leg2.localPosition.y, 0, 0.1f) + (transform.localPosition.z * transform.forward);
-                leg3.localPosition = (transform.localPosition.x * transform.right) + transform.up * AnimMath.Slide(leg3.localPosition.y, 0, 0.1f) + (transform.localPosition.z * transform.forward);
-                leg4.localPosition = (transform.localPosition.x * transform.right) + transform.up * AnimMath.Slide(leg4.localPosition.y, 0, 0.1f) + (transform.localPosition.z * transform.forward);
+                leg1.localPosition = (0.25f * transform.right)  + transform.up * AnimMath.Slide(leg1.localPosition.y, 0, 0.1f) + (0.25f* transform.forward);
+                leg2.localPosition = (-0.25f * transform.right) + transform.up * AnimMath.Slide(leg2.localPosition.y, 0, 0.1f) + (0.25f * transform.forward);
+                leg3.localPosition = (0.25f * transform.right)  + transform.up * AnimMath.Slide(leg3.localPosition.y, 0, 0.1f) + (-0.25f * transform.forward);
+                leg4.localPosition = (-0.25f * transform.right) + transform.up * AnimMath.Slide(leg4.localPosition.y, 0, 0.1f) + (-0.25f * transform.forward);
                 break;
             case "Walk":
-                transform.position = (transform.position.x * transform.right) + (transform.up * AnimMath.Slide(transform.position.y, 1.25f, 0.1f)) + (transform.position.z * transform.forward);
+                transform.position = (Mathf.MoveTowards(transform.position.x, player.position.x, Time.deltaTime) * transform.right) + (transform.up * AnimMath.Slide(transform.position.y, 1.25f, 0.1f)) + (Mathf.MoveTowards(transform.position.z, player.position.z, Time.deltaTime) * transform.forward);
                 leg1.localPosition = (0.25f * transform.right) - (transform.up * ((Mathf.Sin(Time.time*25)+1)/2)) + (0.25f* transform.forward);
                 leg2.localPosition = (-0.25f * transform.right) - (transform.up * ((Mathf.Cos(Time.time*25)+1)/2)) + (0.25f * transform.forward);
                 leg3.localPosition = (0.25f * transform.right) - (transform.up * ((Mathf.Sin(Time.time*25 + 1)+1)/2)) + (-0.25f * transform.forward);
                 leg4.localPosition = (-0.25f * transform.right) - (transform.up * ((Mathf.Cos(Time.time*25 + 1)+1)/2)) + (-0.25f * transform.forward);
                 break;
             case "Fire":
+                Vector3 disToTarget = player.position - transform.position;
+                rotator.rotation = AnimMath.Slide(rotator.rotation, Quaternion.LookRotation(disToTarget, -Vector3.up));
                 break;
             case "Destroyed":
                 break;
